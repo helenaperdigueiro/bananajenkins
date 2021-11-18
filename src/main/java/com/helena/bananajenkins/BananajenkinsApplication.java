5fb1f21b9c1ed406b97911fad64751f5cd3216d0
helenaperdigueiro <helena.perdigueiro@gmail.com>
2021-11-18 11:10:09 -0300

    criando banana jenkins

---
+++ src/main/java/com/helena/bananajenkins/BananajenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.helena.bananajenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananajenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananajenkinsApplication.class, args);
+	}
+
+}
